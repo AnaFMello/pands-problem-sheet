24b93b4b5a56d9e5e2dae9f8390af61db85400f7
Ana Mello <G00425653@atu.ie>
2023-03-15 19:37:29 +0000

    Today is:

--- week 5/weekday.py
+++ week 5/weekday.py
@@ -1,8 +1,11 @@
-# This program outputs whether or not today is a weekday.
+# This program outputs whether or not today is a weekday. 
 
+# first I have to import the date
 
 from datetime import date
 week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+
+# now, using if, else, it prints todays date
 
 print ("weekday!" 
        if (week[date.today().weekday()]) in (0, 1, 2, 3, 4, 5) 
