b0f4df7e8cf41627c17ab90182ca24fe02cb4e0c
Ana Mello <G00425653@atu.ie>
2023-04-15 16:10:10 +0100

    problem sheet

---
+++ bank.py
@@ -0,0 +1,17 @@
+
+number1 = input("Enter the first amoun: ")
+number2 = input("Enter the second amount: ")
+
+number1 = float(number1)
+number2 = float(number2)
+
+number1 = number1 / 100
+number2 = number2 / 100
+
+added = float(number1 + number2)
+print("Your amount total is €",added)
+
+
+## After your feedback I did a reasearch and I did not find a way to get this resul without / 100
+## As pm running to get it done and focus on the project, I´ll leave as it is, but I will research better during the hollidays
+## Thank you!! 
